8e97856ee904bc42413b51ce057257bd0ac0f66c
orlandofat <orlandofat@gmail.com>
2020-08-25 10:31:29 -0400

    upload

---
+++ js/app.js
@@ -0,0 +1,19 @@
+const search = new Filter("search", "data-caption");
+baguetteBox.run(".gallery");
+
+// let input = document.querySelector("input");
+// let listItems = document.querySelectorAll("a");
+// input.addEventListener("input", filter);
+
+// function filter() {
+//   search = input.value.toLowerCase();
+
+//   listItems.forEach(function (a) {
+//     text = a.innerHTML.toLowerCase();
+//     if ((search = "" || found != -1)) {
+//       a.style.display = "block";
+//     } else {
+//       a.style.display = "none";
+//     }
+//   });
+// }
